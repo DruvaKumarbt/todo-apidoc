2e868aaec63b0304961e0e1fa50c0117c009e342
Druva Kumar <druvakumar.bt@gmail.com>
2020-06-19 13:03:17 +0530

    first-commit

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "ToDo",
+  "version": "0.0.1",
+  "description": "Documentation for the ToDo Application",
+  "title": "ToDo APIs",
+  "url": "http://api.todo-druvakumar.xyz",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-06-17T08:53:10.458Z",
+    "url": "http://apidocjs.com",
+    "version": "0.23.0"
+  }
+});
